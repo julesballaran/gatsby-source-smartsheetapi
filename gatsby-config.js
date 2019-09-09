b8583cf17c4aee912ff6afe5fc831fe9a029d9a8
Jules Ballaran <jules.ballaran@boom.camp>
2019-09-09 09:51:15 +0800

    add env file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 module.exports = {
   plugins: [
     {
       resolve: 'gatsby-source-smartapi',
       options: {
         sheetId: '3940651537590148',
-        tokken: 'hrd26x6opt9manpkxj16gd02ey'
+        tokken: process.env.TOKKEN
       }
     }
   ]
